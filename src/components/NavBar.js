de3272465eac4cd8ea707126d9990ef5e8d03ee1
Awais khatab <42613988+AKhatabdev@users.noreply.github.com>
2021-09-16 10:34:05 +0100

    Landing-page updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,5 @@
+import "../styles/NavBar.css"
+
 import React from 'react';
 import {Button, Navbar, NavbarBrand, Container} from "react-bootstrap";
 import {useHistory} from "react-router-dom"
@@ -8,7 +10,7 @@ export default function NavBar() {
     return (
         <Navbar className="justify-content-between" id="navigation-bar" fixed="top" sticky="top" bg="transparent"
                 variant="dark" fluid>
-            <NavbarBrand className="m-3 fw-bolder">Lorderly</NavbarBrand>
+            <NavbarBrand className="m-3 fw-bolder"><h4>Lorderly</h4></NavbarBrand>
             <Container className="float-end m-3 p-1">
                 <Button onClick={() => history.push('/login')} ClassName="m-3 fw-bolder" type="button"
                         variant="outline-primary">Login</Button>{' '}
