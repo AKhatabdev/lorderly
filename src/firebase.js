0e25f95790fdb5fae84d4215cfff9e3c5b90ec0c
Awais khatab <42613988+AKhatabdev@users.noreply.github.com>
2021-09-23 00:18:00 +0100

    Changed directory

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import firebase from "firebase/compat";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    dataBaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: 'AIzaSyCUJ3uIzvzRndzd4xFgjOO7Jxg8ynf6tDY',
+    authDomain: 'lorderly-auth-dev.firebaseapp.com',
+    dataBaseURL: 'https://lorderly-auth-dev-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'lorderly-auth-dev',
+    storageBucket: 'lorderly-auth-dev.appspot.com',
+    messagingSenderId: '988399499370',
+    appId: '1:988399499370:web:2e8ebe9cf91d54dfb63213'
 })
 
 export const auth = app.auth()
 export default app
+
